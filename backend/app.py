eb2f92368517ec8a64b1ff938a1982153e369ae5
pradeep3122003 <pradeepkh312@gmail.com>
2025-10-15 07:21:25 +0000

    app.py initial

---
+++ backend/app.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, jsonify
+from flask_cors import CORS
+from dotenv import load_dotenv
+from config import Config
+from extension import db, jwt
+
+load_dotenv()
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    CORS(app, supports_credentials=True, origins=["http://127.0.0.1:5000"])
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    jwt.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+if __name__=="__main__":
+   host=os.getenv("HOST")
+   port=os.getenv("PORT")
+   debug=True if os.getenv("DEBUG")=="1" else False
+   app = create_app()
+   app.run(debug=debug, host=host, port=port)
